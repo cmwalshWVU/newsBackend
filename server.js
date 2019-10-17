35b782c77cc647e03c1cca0f7c3c02471b1c0a88
colin walsh <colinwalsh@colins-MacBook-Pro.local>
2019-10-16 22:49:35 -0400

    update server to use env vars

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ const topic = 'news';
 const axios = require('axios');
 const app = express();
 
-var serviceAccount = require("./key.json");
+// var serviceAccount = require("./key.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "crypto-watch-dbf71",
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": process.env.AUTH_URI,
+        "token_uri": process.env.TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.envAUTH_PROVIDER_X509_CERT_URL
+      }),
     databaseURL: "https://crypto-watch-dbf71.firebaseio.com"
   });
 
