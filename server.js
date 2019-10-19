81d5f063e84f13f264aea289f6293dcbd4cfe04b
colin walsh <colinwalsh@colins-MacBook-Pro.local>
2019-10-19 12:37:07 -0400

    comment out firebase

--- server.js
+++ server.js
@@ -10,24 +10,25 @@ const topic = 'news';
 const axios = require('axios');
 const app = express();
 
-admin.initializeApp({
-    credential: admin.credential.cert(process.env.FIREBASE_CONFIG),
-    databaseURL: "https://crypto-watch-dbf71.firebaseio.com"
-  });
-
 // admin.initializeApp({
-//     credential: admin.credential.cert({
-//         "type": "service_account",
-//         "project_id": "crypto-watch-dbf71",
-//         "private_key_id": process.env.PRIVATE_KEY_ID,
-//         "private_key": process.env.PRIVATE_KEY,
-//         "client_email": process.env.CLIENT_EMAIL,
-//         "client_id": process.env.CLIENT_ID,
-//         "auth_uri": process.env.AUTH_URI,
-//         "token_uri": process.env.TOKEN_URI,
-//         "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-//         "client_x509_cert_url": process.envAUTH_PROVIDER_X509_CERT_URL
-//       }),
+//     credential: admin.credential.cert(process.env.FIREBASE_CONFIG),
+//     databaseURL: "https://crypto-watch-dbf71.firebaseio.com"
+//   });
+
+// const config  = {
+//     type: "service_account",
+//     project_id: "crypto-watch-dbf71",
+//     private_key_id: process.env.PRIVATE_KEY_ID,
+//     private_key: process.env.PRIVATE_KEY,
+//     client_email: process.env.CLIENT_EMAIL,
+//     client_id: process.env.CLIENT_ID,
+//     auth_uri: process.env.AUTH_URI,
+//     token_uri: process.env.TOKEN_URI,
+//     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+//     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+//   }
+// admin.initializeApp({
+//     credential: admin.credential.cert(config),
 //     databaseURL: "https://crypto-watch-dbf71.firebaseio.com"
 //   });
 
@@ -104,9 +105,9 @@ function fetchPriceData(ticker, numberOfDataPoints) {
     axios.get('https://min-api.cryptocompare.com/data/histominute?fsym=' + ticker + '&tsym=USD&limit=' + numberOfDataPoints + '&aggregate=1&e=CCCAGG')
         .then(response => {
             response.data.Data.map(price => {
-                admin.firestore().collection('priceData').doc('priceHistory').collection(ticker).doc(price.time.toString()).set({
-                    timeStamp: price.time,
-                    price: price })
+                // admin.firestore().collection('priceData').doc('priceHistory').collection(ticker).doc(price.time.toString()).set({
+                //     timeStamp: price.time,
+                //     price: price })
                 pusher.trigger('price-channel', ticker, {
                     prices: price,
                 });
@@ -116,6 +117,7 @@ function fetchPriceData(ticker, numberOfDataPoints) {
 }
 
 function fetchTopCryptos(numberOfDataPoints) {
+    // console.log(JSON.stringify(config))
     axios.get("https://api.coinmarketcap.com/v1/ticker/?limit=20")
         .then(response => {
             var result = response.data.filter(currency => currency.rank <= 10);
